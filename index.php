91127e771d1e19c465d496d680ed3ff4b32981e9
shravanikadam <75267885+shravanikadam@users.noreply.github.com>
2021-10-15 11:54:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta name="viewport" content="width-device-width, initial-scale=1">
-    <title>OLMS-ChatBot</title>
+    <title>OOPS TUTOR-ChatBot</title>
     <!-- dialogflow style bootsrap -->
     <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
     <style>
